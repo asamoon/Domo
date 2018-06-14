2b6b72dd8b453d419b84a304471be1c6c548e596
asamoon <asamoon@gmail.com>
2018-06-14 18:25:18 +0800

    test

---
+++ docs/.vuepress/components/Badge.vue
@@ -0,0 +1,26 @@
+<template>
+  <span :class="['badge', !!type ? 'badge--'+type : '']">
+    <slot></slot>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'badge',
+  props: {
+    type: {
+      type: 'String',
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.badge {
+  background: #999;
+  display: inline-block;
+  border-radius: .3rem;
+  font-size: 1.2rem;
+}
+</style>
